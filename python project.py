6b6e0acf06633a5ccfa9ef672ab33a9f11f3b47e
Pranav16csu256 <38377630+Pranav16csu256@users.noreply.github.com>
2018-04-15 22:14:39 +0530

    Update python project.py

--- python project.py
+++ python project.py
@@ -1,9 +1,13 @@
+#NorthCap University
+#Pranav Garg
+#Under Guidance of Prof.Sandeep Singh
+#A GUI for Customer Managment System
 from tkinter import *
 win=Tk()
-count=0
+count=0                      # Global count variable
 
 
-def first():
+def first():                # Function to traverse first record
     global count
     f=open("python2.txt","r")
     l=f.readlines()
@@ -14,7 +18,7 @@ def first():
     s4.set(m[3])
     s5.set(m[4])
     count=1
-def last():
+def last():                        #Function to traverse last record
     global count
     f=open("python2.txt","r")
     l=f.readlines()
@@ -26,7 +30,7 @@ def last():
     s4.set(m[3])
     s5.set(m[4])
     count=k
-def addrec():
+def addrec():                   #Function to add new record
     flag=0
     s6.set("")
     f=open("python2.txt","r")
@@ -63,7 +67,7 @@ def addrec():
         s6.set("Id already exist")
     f.close()
        
-def nextrec():
+def nextrec():                   # Function to see next  record
     r=""
     s6.set(r)
     global count
@@ -95,7 +99,7 @@ def nextrec():
         
     
     f.close()
-def prev():
+def prev():                  #Function to see previous record
     r=""
     s6.set(r)
     global count
@@ -126,7 +130,7 @@ def prev():
         s6.set("you cant go back")
         count=0
           
-def delrec():
+def delrec():                                              #Function to delete record
     k=[s1.get(),s2.get(),s3.get(),s4.get(),s5.get()]
     f=open("python2.txt","r")
     lines=f.readlines()
@@ -141,7 +145,7 @@ def delrec():
              f.writelines(m[0].ljust(3)+m[1].ljust(20)+m[2].ljust(20)+m[3].ljust(20)+m[4].ljust(5)+"\n")
              s6.set("Record Deleted")
     f.close()
-def search():
+def search():                                #Function to search record
     k=s1.get()
   
     f=open("python2.txt","r")
@@ -158,7 +162,7 @@ def search():
             s5.set(j[4])
             
     f.close()        
-def update():
+def update():                         #Function to update record
     a1=s1.get()
     a2=s2.get()
     a3=s3.get()
@@ -183,7 +187,7 @@ def update():
         
     
     
-def clear():
+def clear():                        #Function to clear record
     global count
     count=0 
     m=""
@@ -193,25 +197,25 @@ def clear():
     s4.set(m)
     s5.set(m)
     s6.set(m)
-s1=StringVar()
+s1=StringVar()           #Declaration of string variable
 s2=StringVar()
 s3=StringVar()
 s4=StringVar()
 s5=StringVar()
 s6=StringVar()
-l1=Label(win,text="CustomerID")
+l1=Label(win,text="CustomerID")        #Declaration of Labels variable
 l2=Label(win,text="Name")
 l3=Label(win,text="Address")
 l4=Label(win,text="Mobile No.")
 l5=Label(win,text="Bill")
-t1=Entry(win,textvariable=s1)
+t1=Entry(win,textvariable=s1)           #Declaration of Textboxes
 t2=Entry(win,textvariable=s2)
 t3=Entry(win,textvariable=s3)
 t4=Entry(win,textvariable=s4)
 t5=Entry(win,textvariable=s5)
 t6=Entry(win,textvariable=s6)
 
-b1=Button(win,text="add",command=addrec)
+b1=Button(win,text="add",command=addrec)               #Declaration of Buttons
 b4=Button(win,text=">",command=nextrec)
 b2=Button(win,text="del",command=delrec)
 b7=Button(win,text="search",command=search)
@@ -220,7 +224,7 @@ b3=Button(win,text="<",command=prev)
 b8=Button(win,text="|<",command=first)
 b9=Button(win,text=">|",command=last)
 b6=Button(win,text="clear",command=clear)
-l1.grid(row=1,column=1)
+l1.grid(row=1,column=1)                        #Using grid layout for GUI interface 
 l2.grid(row=2,column=1)
 l3.grid(row=1,column=3)
 l4.grid(row=2,column=3)
@@ -240,4 +244,4 @@ b7.grid(row=5,column=7)
 b6.grid(row=7,column=3)
 b8.grid(row=7,column=2)
 b9.grid(row=7,column=4)
-win.mainloop()
+win.mainloop()                   #end of program
